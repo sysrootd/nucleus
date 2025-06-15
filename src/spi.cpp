a7096be3d800ceb17394dd0eb40e302036eac141
rakesh <alasyamrakesh77@gmail.com>
2025-06-15 22:25:55 +0530

    spi driver updated

--- src/spi.cpp
+++ src/spi.cpp
@@ -2,33 +2,33 @@
 #include "stm32.hpp"
 
 void SPI_Driver::init(Mode mode, DataSize dataSize, BaudRate baud) {
-    // Enable clock for SPI peripheral
+    // Enable SPI clock
     if (spi_ == SPI1) {
-        RCC->APB2ENR |= RCC_APB2ENR_SPI1EN;
+        *(volatile uint32_t*)0x40023844 |= (1 << 12);  // RCC->APB2ENR |= SPI1EN (bit 12)
     } else if (spi_ == SPI2) {
-        RCC->APB1ENR |= RCC_APB1ENR_SPI2EN;
+        *(volatile uint32_t*)0x40023840 |= (1 << 14);  // RCC->APB1ENR |= SPI2EN (bit 14)
     }
 
-    // Reset SPI
-    spi_->CR1 &= ~SPI_CR1_SPE;  // Disable SPI
-    
+    // Disable SPI
+    spi_->CR1 &= ~(1 << 6);  // SPE = 0
+
     // Configure mode
     switch (mode) {
-        case Mode::Mode0:
-            spi_->CR1 &= ~SPI_CR1_CPOL;
-            spi_->CR1 &= ~SPI_CR1_CPHA;
+        case Mode::Mode0:  // CPOL = 0, CPHA = 0
+            spi_->CR1 &= ~(1 << 1);  // CPOL = 0
+            spi_->CR1 &= ~(1 << 0);  // CPHA = 0
             break;
-        case Mode::Mode1:
-            spi_->CR1 &= ~SPI_CR1_CPOL;
-            spi_->CR1 |= SPI_CR1_CPHA;
+        case Mode::Mode1:  // CPOL = 0, CPHA = 1
+            spi_->CR1 &= ~(1 << 1);  // CPOL = 0
+            spi_->CR1 |=  (1 << 0);  // CPHA = 1
             break;
-        case Mode::Mode2:
-            spi_->CR1 |= SPI_CR1_CPOL;
-            spi_->CR1 &= ~SPI_CR1_CPHA;
+        case Mode::Mode2:  // CPOL = 1, CPHA = 0
+            spi_->CR1 |=  (1 << 1);  // CPOL = 1
+            spi_->CR1 &= ~(1 << 0);  // CPHA = 0
             break;
-        case Mode::Mode3:
-            spi_->CR1 |= SPI_CR1_CPOL;
-            spi_->CR1 |= SPI_CR1_CPHA;
+        case Mode::Mode3:  // CPOL = 1, CPHA = 1
+            spi_->CR1 |=  (1 << 1);  // CPOL = 1
+            spi_->CR1 |=  (1 << 0);  // CPHA = 1
             break;
     }
 
@@ -38,60 +38,46 @@ void SPI_Driver::init(Mode mode, DataSize dataSize, BaudRate baud) {
     // Configure baud rate
     set_baud_rate(baud);
 
-    // Configure as master
-    spi_->CR1 |= SPI_CR1_MSTR;
+    // Set master mode
+    spi_->CR1 |= (1 << 2);  // MSTR = 1
 
     // Enable SPI
-    spi_->CR1 |= SPI_CR1_SPE;
+    spi_->CR1 |= (1 << 6);  // SPE = 1
 }
 
 void SPI_Driver::enable() {
-    spi_->CR1 |= SPI_CR1_SPE;
+    spi_->CR1 |= (1 << 6);  // SPE = 1
 }
 
 void SPI_Driver::disable() {
-    spi_->CR1 &= ~SPI_CR1_SPE;
+    spi_->CR1 &= ~(1 << 6);  // SPE = 0
 }
 
 uint8_t SPI_Driver::transmit_receive(uint8_t data) {
-    // Wait for TX buffer empty
-    while (!(spi_->SR & SPI_SR_TXE));
-    
-    // Send data
-    *((volatile uint8_t*)&spi_->DR) = data;
-    
-    // Wait for RX buffer not empty
-    while (!(spi_->SR & SPI_SR_RXNE));
-    
-    // Return received data
-    return *((volatile uint8_t*)&spi_->DR);
+    while (!(spi_->SR & (1 << 1)));         // Wait until TXE (bit 1) is set
+    *((volatile uint8_t*)&spi_->DR) = data; // Write data
+    while (!(spi_->SR & (1 << 0)));         // Wait until RXNE (bit 0) is set
+    return *((volatile uint8_t*)&spi_->DR); // Read received data
 }
 
 uint16_t SPI_Driver::transmit_receive(uint16_t data) {
-    // Wait for TX buffer empty
-    while (!(spi_->SR & SPI_SR_TXE));
-    
-    // Send data
+    while (!(spi_->SR & (1 << 1)));         // TXE
     spi_->DR = data;
-    
-    // Wait for RX buffer not empty
-    while (!(spi_->SR & SPI_SR_RXNE));
-    
-    // Return received data
+    while (!(spi_->SR & (1 << 0)));         // RXNE
     return spi_->DR;
 }
 
 void SPI_Driver::set_data_size(DataSize size) {
-    spi_->CR1 &= ~SPI_CR1_DFF;  // Clear DFF bit
-    
     if (size == DataSize::Bits16) {
-        spi_->CR1 |= SPI_CR1_DFF;
+        spi_->CR1 |= (1 << 11);  // DFF = 1
+    } else {
+        spi_->CR1 &= ~(1 << 11); // DFF = 0
     }
 }
 
 void SPI_Driver::set_baud_rate(BaudRate rate) {
-    spi_->CR1 &= ~SPI_CR1_BR;  // Clear baud rate bits
-    
+    spi_->CR1 &= ~(0b111 << 3);  // Clear BR[2:0] bits
+
     switch (rate) {
         case BaudRate::Div2:   spi_->CR1 |= (0b000 << 3); break;
         case BaudRate::Div4:   spi_->CR1 |= (0b001 << 3); break;
