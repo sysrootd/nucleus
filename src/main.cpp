d134c7ebe8d01ee8c1a693852c4f8406c861b967
rakesh <alasyamrakesh77@gmail.com>
2025-07-19 00:31:53 +0530

    main.cpp updated with stack size

--- src/main.cpp
+++ src/main.cpp
@@ -21,29 +21,24 @@ void led_task_2() {
     }
 }
 
-// constexpr size_t STACK_WORDS = 512;  // 512 * 4B = 2KB
-// alignas(8) static uint32_t stack1[STACK_WORDS];
-// alignas(8) static uint32_t stack2[STACK_WORDS];
+constexpr size_t STACK_WORDS = 512;  // 512 * 4B = 2KB
+alignas(8) static uint32_t stack1[STACK_WORDS];
+alignas(8) static uint32_t stack2[STACK_WORDS];
 
 int main() {
     static GPIO gpio(GPIOB);
     gpioB = &gpio;
+
     SysTick_Init();
+    Scheduler::init();
 
-    // SysTick_Init();
-    // Scheduler::init();
+    static Thread t1(led_task_1, 1, &stack1[STACK_WORDS - 1], STACK_WORDS);
+    static Thread t2(led_task_2, 1, &stack2[STACK_WORDS - 1], STACK_WORDS);
 
-    // static Thread t1(led_task_1, 1, stack1, STACK_WORDS);
-    // static Thread t2(led_task_2, 1, stack2, STACK_WORDS);
+    Scheduler::add_thread(t1.get_tcb());
+    Scheduler::add_thread(t2.get_tcb());
 
-    // Scheduler::add_thread(t1.get_tcb());
-    // Scheduler::add_thread(t2.get_tcb());
+    Scheduler::start();
 
-    // Scheduler::start();
-
-    while (true){
-        gpioB->toggle(GPIOPin ::Pin13);
-        delay_ms(1000);
-        gpioB->toggle(GPIOPin ::Pin14);
-    }
+    while (true);
 }
